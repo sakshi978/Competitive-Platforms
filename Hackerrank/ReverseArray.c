abf87c4e402faf4198ab40db092713f3b1ac71f1
sakshi978 <57284653+sakshi978@users.noreply.github.com>
2020-05-01 17:50:07 +0530

    Create ReverseArray.c

---
+++ Hackerrank/ReverseArray.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+
+    /* Write the logic to reverse the array. */
+    if((num>=1) && (num<=1000))
+    {
+        for(i = num-1; i >= 0; i--)
+        {
+            printf("%d ", *(arr + i));
+        }
+    }
+
+    return 0;
+}
