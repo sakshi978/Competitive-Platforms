0a1c1a54119d7bb6f5c7747a4c7a7fd8df51e2a7
sakshi978 <57284653+sakshi978@users.noreply.github.com>
2020-06-10 14:30:35 +0530

    Create ArithmeticOperators.py

---
+++ Hackerrank/ArithmeticOperators.py
@@ -0,0 +1,34 @@
+"""
+Question:
+Task
+Read two integers from STDIN and print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+
+Input Format
+
+The first line contains the first integer, a. The second line contains the second integer,b .
+
+Output Format
+Print the three lines as explained above.
+
+Sample Input 0
+3
+2
+Sample Output 0
+5
+1
+6
+"""
+def main():
+    a = int(input())
+    b = int(input())
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+if __name__ == '__main__':
+    main()
