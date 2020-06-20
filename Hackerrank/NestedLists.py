1dcdbfe105eab684059412b55485b551cc464e6d
sakshi978 <57284653+sakshi978@users.noreply.github.com>
2020-06-20 18:24:38 +0530

    Create NestedLists.py

---
+++ Hackerrank/NestedLists.py
@@ -0,0 +1,82 @@
+'''
+Question:
+Given the names and grades for each student in a Physics class of N students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+Note: If there are multiple students with the same grade, order their names alphabetically and print each name on a new line.
+
+Input Format
+
+The first line contains an integer,N , the number of students.
+The 2N subsequent lines describe each student over 2 lines; the first line contains a student's name, and the second line contains their grade.
+
+Constraints
+2<=N<=5
+There will always be one or more students having the second lowest grade.
+
+Output Format
+Print the name(s) of any student(s) having the second lowest grade in Physics; if there are multiple students, order their names alphabetically and print each one on a new line.
+
+Sample Input 0
+5
+Harry
+37.21
+Berry
+37.21
+Tina
+37.2
+Akriti
+41
+Harsh
+39
+
+Sample Output 0
+Berry
+Harry
+
+Explanation 0
+There are 5 students in this class whose names and grades are assembled to build the following list:
+
+python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
+The lowest grade of 37.2 belongs to Tina. The second lowest grade of 37.21 belongs to both Harry and Berry, so we order their names alphabetically and print each name on a new line.
+'''
+if __name__ == '__main__':
+    lst = list()
+    for i in range(int(input())):
+        name = input()
+        score = float(input())
+
+        ele = [name,score]
+        lst.append(ele)
+
+    #print(lst)
+    #print(lst[0][1])
+    min1 = lst[0][1]
+    
+    index = 0
+
+    for i in range(len(lst)):
+        #print(lst[i][1])
+        if(lst[i][1] < min1):
+            min1 = lst[i][1]
+            index = i
+
+    #print(min1)
+    lst.remove(lst[index])
+    #print(lst)
+
+    min2 = lst[0][1]
+
+    for i in range(len(lst)):
+
+        if(lst[i][1] < min2):
+            min2 = lst[i][1]
+
+    #print(min2)
+
+    lst.sort(key = lambda x: x[0]) 
+    #print(lst)
+
+    for i in range(len(lst)):
+        if(lst[i][1] == min2):
+            print(lst[i][0])
